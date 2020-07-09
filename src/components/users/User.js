3ef1586b90ab1dc04a96a124b9ad9f123bbc1884
karim amzil <karimamzil19@gmail.com>
2020-07-09 17:14:10 +0100

    ajout de component user

---
+++ src/components/users/User.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+class User extends Component {
+    render() {
+        const{login,avatar_url,html_url,repos_url}=this.props.user;
+
+        return (
+            <div>
+                 <div className="card">
+                  <img className="card-img-top" src={avatar_url}  alt=""/>
+                  <div className="card-body">
+                     <h4 className="card-title">{login}</h4>
+                            <p className="card-text">
+                          <Link to={'/users/'+login} className="btn btn-success">Show more</Link>
+                         
+                           </p>
+                   </div>
+                 </div> 
+            </div>
+        )
+    }
+}
+export default User;
