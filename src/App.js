3c3f2c3f3186a13e9ca77c81e908d2df545a9408
karim amzil <karimamzil19@gmail.com>
2020-07-09 17:08:10 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import Users from './components/users/Users'
 import './App.css';
-
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import UserItem from './components/users/UserItem'
+import {Link} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+        <div className="App">
+            <nav className="navbar navbar-expand navbar-dark btn btn-primary " >
+               <Link  className="btn btn-dark" to="#">GitHub accounts</Link>
+            </nav>
+            <div className="container mt-2">
+              <Switch>
+                 <Route exact path="/users"component={Users}/>
+                 <Route exact path="/users/:login"component={UserItem}/>
+                 <Route exact component={Users}/>
+              </Switch>
+            </div>
+        </div>
+    </Router>
+      
   );
 }
 
